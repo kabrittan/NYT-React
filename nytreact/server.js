db8b29da443d0358d4de6500f61a9f9da872384f
Kyle Brittan <kabrittan@gmail.com>
2018-06-24 16:33:42 -0700

    Added Server

---
+++ nytreact/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api-routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// Routes
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nytsearch');
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
